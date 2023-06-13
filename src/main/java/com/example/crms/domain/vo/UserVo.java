54d79d4e2750ab5bb2bc463bb68e75cdca25d862
zhaotong <dida@qq.com>
2023-06-13 13:33:17 +0800

    2023/6/13_zhaotong

---
+++ src/main/java/com/example/crms/domain/vo/UserVo.java
@@ -0,0 +1,20 @@
+package com.example.crms.domain.vo;
+
+import lombok.Data;
+
+
+@Data
+public class UserVo {
+
+    //用户ID
+    private Integer userId;
+    //用户姓名
+    private String userName;
+    //用户邮箱
+    private String userEmail;
+    //用户部门Id
+    private Integer departmentId;
+    //用户部门名称
+    private String departmentName;
+
+}
